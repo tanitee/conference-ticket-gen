bd299e9024c7553d9baecdcd716d005047bb78c5
tanitee <tanitee13@gmail.com>
2025-09-03 16:32:56 +0100

    added validation logic with a RHF

--- ticket-gen/src/components/Form.jsx
+++ ticket-gen/src/components/Form.jsx
@@ -1,28 +1,15 @@
-import {useState} from 'react'
+import {useState , useEffect } from 'react'
 import uploadIcon from '../assets/images/icon-upload.svg'
-
+import {useForm} from 'react-hook-form'
+import iconInfo from '../assets/images/icon-info.svg'
 
 const Form = () => {
+    const { register, handleSubmit, formState: { errors }} = useForm();
 
-    const [name, setName] = useState(" ")
-    const [email, setEmail] = useState(" ")
-    const [github, setGithub] = useState(" ")
-    const [file, setFile] = useState(null)
+    //console.log(register)
 
-    const handleNameChange = (e) => {
-        setName(e.target.value)
-    }
-
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
-    }
-
-    const handleGithubChange = (e) => {
-        setGithub(e.target.value)
-    }
-
-    const handleFileChange = (e) => {
-        setFile(e.target.files[0])
+    const validateForm = (data)=>{
+      console.log(data);
     }
 
     const styles ={
@@ -35,7 +22,7 @@ const Form = () => {
 
   return (
     <>
-        <form id="customer-form">
+        <form id="customer-form" onSubmit={handleSubmit(validateForm)}>
           
           <div className="upload-container">
             <p style={style2}>Upload Avatar</p>
@@ -43,33 +30,73 @@ const Form = () => {
               <img src={uploadIcon} alt="Upload Icon"/>
               <span>Drag and drop or click to upload</span>
             </label>
-            <input type="file" name="filename" id="file-upload"/>
+            <input type="file" name="filename" id="file-upload"
+              {...register("file",{
+                required:"Please upload a file",
+                validate:(files)=>{
+                  return files && files[0]?.size <= 500 * 1024 || "File too large. Please upload a photo less than 500kb"
+                }
+              })}
+            />
             <div className="upload-info">
               <img src="./assets/images/icon-info.svg" alt=""/>
               <span style={styles}>Upload your photo (JPG or PNG, max size: 500kb)</span>
             </div>
-            <div className="error"></div>
+            {errors.file && <div className="error-message">
+                <img src={iconInfo} className='icon-img' alt=""/>  {errors.file.message} 
+                </div> }
           </div>
 
             <div className="name-container">
               <label htmlFor="name">Full Name</label>
-              <input type="text" id="name" name="name" value={name} onChange={handleNameChange}/>
-              <div className="error"></div>
+              <input type="text" id="name" name="name"
+                className={errors.name ? "error-border" : " " }
+                {...register("name", {
+                  required: "Full name is required",
+                  minLength: {value: 3 , message: "must be atleast 3 characters"},
+                  validate: (value) => value.trim() !== "" || "Full name cannot be just spaces"
+                })}
+              />
+
+              {errors.name && <div className="error-message">
+                <img src={iconInfo} className='icon-img' alt=""/>  {errors.name.message} 
+                </div>
+              }
             </div> 
 
             <div className="email-cont">
               <label htmlFor="email">Email Address</label>
-              <input type="email" placeholder="example@email.com" id="email" name="email" value={email} onChange={handleEmailChange}/>
-              <div className="error"></div>
+              <input type="email" placeholder="example@email.com" id="email" name="email"
+                className={errors.name ? "error-border" : " " }
+                {...register("email", {
+                  required: "Please enter your email",
+                  pattern:{
+                    value:/^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message:"Please enter a valid email address"
+                  }
+                })}
+              />
+              {errors.email && <div className="error-message">
+                <img src={iconInfo} className='icon-img' alt=""/>  {errors.email.message} 
+                </div> 
+              }
             </div>
 
             <div className="github-info-cont">
               <label htmlFor="github">GitHub Username</label>
-              <input type="text" placeholder="@yourusername" id="github" name="github" value={github} onChange={handleGithubChange}/>
-              <div className="error"></div>
+              <input type="text" placeholder="@yourUsername" id="github" name="github"
+                className={errors.name ? "error-border" : " " }
+                {...register("github",{
+                  required:"Please enter your github username",
+                  minLength:{value:3, message:"must be atleast 3 characters"}
+                })}
+              />
+              {errors.github && <div className="error-message">
+                <img src={iconInfo} className='icon-img' alt=""/>  {errors.github.message} 
+                </div>  }
             </div>
 
-             <button className="generate-btn" type="submit">Generate My Ticket</button>
+            <button className="generate-btn" type="submit">Generate My Ticket</button>
            
          
         </form>
